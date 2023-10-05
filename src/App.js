18465f27cec966c85cdf0c3e7f7bd5b75eee1763
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-05 10:04:37 +0200

    changed local storage, added padding to header

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import Header from "./Header";
 import Container from "./Container";
 import { useEffect, useState } from "react";
 
-const getInitialTasks = () => {
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-  return tasksFromLocalStorage
-    ? JSON.parse(tasksFromLocalStorage)
-    : [];
-};
+const getInitialTasks = () => (
+  JSON.parse(localStorage.getItem("tasks")) || []
+);
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
