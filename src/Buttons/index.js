b962f04201d9695c00b56ac11c8c038a7d53cacf
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-06 16:45:54 +0200

    added styled components to buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,21 +1,53 @@
-import "./style.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 5px;
+  margin: 5px;
+  color: teal;
+  border-style: none;
+  background-color: whitesmoke;
+  transition: 1s;
+
+  &:hover {
+    color: hsl(180, 100%, 35%);
+  }
+  &:disabled {
+    color: hsl(0, 0%, 40%);
+  }
+
+  @media(max-width: 767px) {
+    margin: 5px;
+  }
+`;
+
+const Div = styled.div`
+  margin: 5px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media(max-width: 767px) {
+    display: grid;
+      grid-template-columns: auto;
+      grid-template-rows: 1fr 1fr;
+  }
+`;
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <div className="buttons">
+  <Div>
     {tasks.length > 0 && (
       <>
-        <button onClick={toggleHideDone} className="buttons_button">
+        <Button onClick={toggleHideDone}>
           {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </button>
-        <button
-          className="buttons_button"
+        </Button>
+        <Button
           disabled={tasks.every(({ done }) => done)}
           onClick={setAllDone}
         >
           Ukończ wszystkie
-        </button>
+        </Button>
       </>
     )}
-  </div>
+  </Div>
 );
 export default Buttons;
