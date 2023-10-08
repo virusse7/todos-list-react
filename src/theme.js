14283a7d309bdc85301ff0172e7116c28ee24610
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-08 15:01:25 +0200

    added colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,17 @@
 export const theme = {
     color: {
-
+        teal: "#008080",
+        brighterteal: "#00a3a3",
+        whitesmoke: "#f5f5f5",
+        bondiblue: "#00b3b3",
+        gray: "#808080",
+        silver: "#cccccc",
+        gainsboro: "#dcdcdc",
+        white: "#ffffff",
+        green: "#00b300",
+        brightergreen: "#00cc00",
+        red: "#e60000",
+        torchred: "#ff1a1a",
     },
     breakpoint: {
         mobileMax: 767,
