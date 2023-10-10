34d46370dd1c513da0e9528bfc6dd65c8e6f90d7
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-10 12:55:21 +0200

    added provider and store

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import reportWebVitals from './reportWebVitals';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
