e6d12b5522491d94f163dc2cb04a55e5953ccfc8
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-10 17:39:24 +0200

    build redux tasksSlice

--- src/features/task/tasksSlice.js
+++ src/features/task/tasksSlice.js
@@ -4,14 +4,44 @@ const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
         tasks: [],
+        hideDone: false,
     },
     reducers: {
-        addTask: ({ tasks }, { payload }) => {
-            tasks.push(payload);
-        }
-    }
+        addTask: ({ tasks }, { payload: task }) => {
+            tasks.push(task);
+        },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks[index].done = !tasks[index].done;
+        },
+        removeTask: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks.splice(index, 1);
+        },
+        setAllDone: ({ tasks }) => {
+            for (const task of tasks) {
+                task.done = true;
+            }
+        },
+    },
 });
 
-export const { addTask } = tasksSlice.actions;
-export const selectTasks = state => state.tasks;
+export const {
+    addTask,
+    toggleTaskDone,
+    toggleHideDone,
+    removeTask,
+    setAllDone,
+} = tasksSlice.actions;
+
+export const selectTasksState = state => state.tasks;
+
+export const selectTasks = state => selectTasksState(state).tasks;
+export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
+export const selectAreTaskEmpty = state => selectTasks(state).length === 0;
+export const selectHideDone = state => selectTasksState(state).hideDone;
+
 export default tasksSlice.reducer;
