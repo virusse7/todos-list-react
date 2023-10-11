371f1d35631a3d9639cb05166e6c7e07357c47b4
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-11 16:23:50 +0200

    renamed watchfetchexample task to tasksSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,8 @@
 import { all } from "redux-saga/effects";
-import { watchFetchExampleTasks } from "./features/task/tasksSaga";
+import { tasksSaga } from "./features/task/tasksSaga";
 
 export default function* rootSaga() {
     yield all([
-        watchFetchExampleTasks(),
+        tasksSaga(),
     ])
 }
