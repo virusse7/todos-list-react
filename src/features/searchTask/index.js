338a28b77ca59f8973d6bc119fc44a88d8360300
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-16 17:13:06 +0200

    added search input to filtr tasks

--- src/features/searchTask/index.js
+++ src/features/searchTask/index.js
@@ -1,10 +1,32 @@
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom";
 import { StyledInput, Div } from "./styled";
 
 
+
 export const SearchContainer = () => {
+    const location = useLocation();
+    const query = (new URLSearchParams(location.search)).get("szukaj");
+    const history = useHistory();
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target.value.trim() === "") {
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+
     return (
         <Div>
-            <StyledInput placeholder="Wpisz zadanie do wyszukania" />
+            <StyledInput
+                placeholder="Filtruj zadania"
+                value={query || ""}
+                onChange={onInputChange}
+            />
         </Div>
     )
 }
