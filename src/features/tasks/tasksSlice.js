b2c37ec518e606332ccd4e8828ee767d99802196
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-16 08:55:56 +0200

    added get task by id in tasks slice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -52,4 +52,6 @@ export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }
 export const selectAreTaskEmpty = state => selectTasks(state).length === 0;
 export const selectHideDone = state => selectTasksState(state).hideDone;
 
+export const getTaskById = (state, taskId) => selectTasks(state).find(({ id }) => id === taskId);
+
 export default tasksSlice.reducer;
