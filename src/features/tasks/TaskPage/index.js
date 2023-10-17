c94e7317718d4581324f4003e6a48a74d044cc51
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-17 11:58:43 +0200

    added styled to task page and edited section

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -2,17 +2,27 @@ import { useSelector } from "react-redux";
 import Section from "../../../common/Section";
 import { useParams } from "react-router-dom";
 import { getTaskById } from "../tasksSlice";
+import { Container } from "../../../common/Container/styled";
+import { Content } from "./styled";
 
 function TaskPage() {
     const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id));
     return (
-        <Section
-            title={task ? task.content : "Nie znaleziono zadania"}
-            body={<><strong>Ukończono: </strong> {task.done ? "Tak" : "Nie"}</>}
-        >
+        <Container>
+            <Section
+                title={`Szczegóły zadania: ${task ? task.content : "Nie znaleziono zadania"}`}
+                body={!!task && (
+                    <>
+                        <Content>
+                            <strong>Ukończono</strong>: {task.done ? "Tak" : "Nie"}
+                        </Content>
+                    </>
+                )}
+            >
+            </Section>
+        </Container>
 
-        </Section>
     )
 }
 
---
+++ src/features/tasks/TaskPage/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+    padding: 20px;
+    font-size: 18px;
+`;
