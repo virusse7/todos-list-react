c22cb8af67d2461531d0f8254dc495beb8555899
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-17 11:50:41 +0200

    built example tasks button

---
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { selectLoading, fetchExampleTasks } from "../../tasksSlice";
+import { ExampleTasksButton } from "../Buttons/styled";
+
+export default () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(selectLoading);
+    return (
+        <ExampleTasksButton disabled={loading} onClick={() => dispatch(fetchExampleTasks())}>
+            {
+                loading ? "Pobieranie..." : "Pobierz przykładowe zadania"
+            }
+        </ExampleTasksButton>
+    );
+}
